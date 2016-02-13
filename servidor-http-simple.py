c01669d5455a42cdc321a8394a9992a9ad6ef531
Cristina Gallego <cristina@ubuntu>
2016-02-13 12:41:45 +0100

    Ejercicio 14.1 IP y puerto

--- servidor-http-simple.py
+++ servidor-http-simple.py
@@ -16,7 +16,7 @@ import socket
 # let's use one above 1024
 
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mySocket.bind(('localhost', 1234))
+mySocket.bind(('192.168.161.146', 2312))
 
 # Queue a maximum of 5 TCP connection requests
 
@@ -24,13 +24,18 @@ mySocket.listen(5)
 
 # Accept connections, read incoming data, and answer back an HTML page
 #  (in an infinite loop)
-
-while True:
-    print 'Waiting for connections'
-    (recvSocket, address) = mySocket.accept()
-    print 'HTTP request received:'
-    print recvSocket.recv(1024)
-    recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
-                    "<html><body><h1>Hello World!</h1></body></html>" +
+try:
+    while True:
+        print 'Waiting for connections'
+        (recvSocket, address) = mySocket.accept()
+        IP = address[0]
+        port = address[1]
+        print 'HTTP request received:'
+        print recvSocket.recv(1024)
+        recvSocket.send("HTTP/1.1 200 OK\r\n\r\n" +
+                    "<html><body><h1>"+"Hola!, Eres la IP: " + str(IP)+" ,en el puerto: "+ str(port)+ "</h1></body></html>" +
                     "\r\n")
-    recvSocket.close()
+        recvSocket.close()
+except KeyboardInterrupt:
+     print "Servidor interrumpido"
+     mySocket.close()
